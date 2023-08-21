dead396f6b166c6b026c3b51d9db8787e0cae288
ahmed <ahmed.hassan.jp@gmail.com>
2023-08-21 21:53:38 +0300

    Add color buttons, linked only black

--- script.js
+++ script.js
@@ -20,15 +20,22 @@ function createGrid(finalNumber) {
 
 
   // Apply the hover effect
-const gridCells = document.querySelectorAll('#grid-cell');
-gridCells.forEach((cell) => {
-    cell.addEventListener('mouseenter', () => {
-      cell.style.backgroundColor = "white";
+  // Black
+  const blackBtn = document.getElementById('black');
+  var blackStyles = {
+    "color": "white",
+    "background-color": "black"
+  };
+
+  blackBtn.addEventListener('click', () => {
+    Object.assign(blackBtn.style, blackStyles);
+    const gridCells = document.querySelectorAll('#grid-cell');
+    gridCells.forEach((cell) => {
+      cell.addEventListener('mouseenter', () => {
+        cell.style.backgroundColor = "black";
+      });
     });
-    cell.addEventListener('mouseleave', () => {
-      cell.style.backgroundColor = "black";
-    });
-});
+  })
 }
 
 // Create a button functionality to manipulate squares values in both sides
@@ -36,11 +43,16 @@ const button = document.getElementById('prompt');
 button.addEventListener('click', () => {
   decideNumber();
   function decideNumber() {
-    const promptAnswer = prompt("Enter squares numbers per side:");
+    const promptAnswer = prompt("Enter squares numbers per side:", 16);
     let finalNumber = Number(promptAnswer);
     console.log(typeof(finalNumber));
     if (Number.isInteger(finalNumber)) {
-      createGrid(finalNumber);
+      if (finalNumber <= 100) {
+        createGrid(finalNumber);
+      } else {
+        alert("Maximum number is 100!")
+        decideNumber()
+      }
     } else {
       alert("Please enter a number!")
       decideNumber();
@@ -48,13 +60,5 @@ button.addEventListener('click', () => {
   }
 });
 
-
-
-// AN IDEA FOR CREATING A ROW AND A COLUMN WITH THE SAME NUMBER
-// - We pass a number (lets say four)
-// - JS takes this number and creates some div elements in that number (now we have four divs)
-// - We style those divs with flexbox to be next each other (for ex: ---- as four divs)
-// - We repeat that process the exact same number of times (for ex: we repeat it four times)
-// - Now we have 2 flexboxes: 
-//    First one for the first row (container) and the divs inside it (flex items)
-//    Second one for the whole rows that we repeated (list items now become the horizontal containers from above ----)
+createGrid(16);
+alert("Pick a color!");
